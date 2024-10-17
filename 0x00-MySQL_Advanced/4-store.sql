cc9746104776d768724abf9871081fb3cc660239
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-17 19:31:34 +0300

    4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+DELIMITER //
+CREATE Trigger update_items
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END //
+DELIMITER ;
