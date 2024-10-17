e513fe021d9c79706f0d084d010a5eb92ac537b1
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-17 21:33:38 +0300

    edit

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,7 +3,8 @@ DELIMITER //
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
     DECLARE project_id INT;
-    SELECT id FROM projects AS project_id
+    SELECT id INTO project_id
+    FROM projects
     WHERE name = project_name;
     IF project_id IS NULL THEN
         INSERT INTO projects (name) VALUES (project_name);
