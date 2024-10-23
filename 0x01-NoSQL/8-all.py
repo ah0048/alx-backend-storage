2cee54bd4d929190fac62bdb47698f846a7e66c7
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-23 13:04:15 +0300

    8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+'''Python function that lists all documents in a collection'''
+
+
+def list_all(mongo_collection):
+    '''Function that lists all documents in a collection'''
+    docs = list(mongo_collection.find())
+    if len(docs) == 0:
+        return []
+    return docs
