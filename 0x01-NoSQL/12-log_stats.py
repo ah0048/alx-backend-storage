5050cd540ae3a502aca7bf877c0152b89d9075ce
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-23 14:16:50 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,14 +3,16 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-logs = client.logs.nginx
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-total_num = logs.count_documents({})
-methods_stats = {m: logs.count_documents({"method": m}) for m in method}
-status_check = logs.count_documents({"method": "GET", "path": "/status"})
-print(f"{total_num} logs")
-print("Methods:")
-for m, c in methods_stats.items():
-    print(f"\tmethod {m}: {c}")
-print(f"{status_check} status check")
+if __name__ == "__main__":
+    '''script that provides some stats about Nginx logs stored in MongoDB'''
+    client = MongoClient()
+    logs = client.logs.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    total_num = logs.count_documents({})
+    methods_stats = {m: logs.count_documents({"method": m}) for m in method}
+    status_check = logs.count_documents({"method": "GET", "path": "/status"})
+    print(f"{total_num} logs")
+    print("Methods:")
+    for m, c in methods_stats.items():
+        print(f"\tmethod {m}: {c}")
+    print(f"{status_check} status check")
