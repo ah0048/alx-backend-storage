26f2917510ee9b4319cd498bfe83e74aadc6a7e1
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-24 15:22:27 +0300

    0 edit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,17 +2,17 @@
 '''Create a Cache class'''
 import redis
 import uuid
-from typing import Union
+from typing import Any
 
 
 class Cache:
     '''Cache class'''
     def __init__(self):
         '''Constructor'''
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
-    def store(self, data: Union[str, float, bytes, int]) -> str:
+    def store(self, data: Any) -> str:
         '''Method that takes a data argument and returns a string'''
         key = str(uuid.uuid4())
         self.__redis.set(key, data)
