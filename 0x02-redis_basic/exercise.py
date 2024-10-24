75acf7b291db615ff62445e4c16d36366526ac67
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-24 19:23:30 +0300

    1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''Create a Cache class'''
 import redis
 import uuid
-from typing import Any, Union
+from typing import Any, Union, Optional, Callable
 
 
 class Cache:
@@ -17,3 +17,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        '''a get method that take a key string argument and a Callable.'''
+        value = self._redis.get(key)
+        if not value:
+            return None
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_str(self, key):
+        '''Method that takes a value and returns a string'''
+        value = self._redis.get(key)
+        if not value:
+            return None
+        return value.decode('utf-8')
+
+    def get_int(self, key):
+        '''Method that takes a value and returns an integer'''
+        value = self._redis.get(key)
+        if not value:
+            return None
+        return int(value)
